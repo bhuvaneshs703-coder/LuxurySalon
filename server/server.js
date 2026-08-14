980c1eb3271936d6c1ffe2a2f4ccc927dc46d8a3
Bhuvanesh <bhuvaneshs709@gmail.com>
2026-08-14 14:03:28 +0530

    Prepare app for deployment

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ const path = require("path");
 
 const app = express();
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 // ==========================
 // ADMIN LOGIN DETAILS
 // ==========================
@@ -436,7 +435,7 @@ app.post(
 // ==========================
 
 app.listen(
-    PORT,
+    PORT,"0.0.0.0"
     () => {
 
         console.log(
